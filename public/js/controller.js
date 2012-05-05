7976c888a2fd4a6597430658f4ce9f1f1e36d218
Grad van Horck <github@gradje.nl>
2012-05-05 19:51:16 +0200

    Added basic bootstrap theme to controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -9,8 +9,15 @@ define([
         _transition: undefined,
 
         _onPluginsLoaded: function() {
+            var self = this;
+
             this._loadProjects();
             this._loadProjectInfo();
+
+            this._connector.addEvent('setScreen', function(screenName, options) {
+                self._onScreenChange(screenName, options);
+            });
+
         },
 
         _loadProjects: function() {
@@ -23,20 +30,40 @@ define([
             // project name
             $$('#projects .name').set('text', this.options.project);
 
-            var list = $('screenList');
+            var screenList = $('screenList');
             // screens:
             Object.each(assetManager.screens, function(config, screenName) {
                 var btn = new Element('button', {
                     text: screenName,
+                    'class': 'btn' + (self.options.currentScreen == screenName ? ' btn-success' : ''),
+                    'data-screen': screenName,
                     events: {
                         click: function() {
+                            this.removeClass(' btn-success');
+                            this.addClass(' btn-warning');
                             self.setScreen(screenName);
                         }
                     }
                 });
                 var listItem = new Element('li');
 
-                listItem.grab(btn).inject(list);
+                listItem.grab(btn).inject(screenList);
+            });
+
+            var overlayList = $('overlayList');
+            Object.each(assetManager.overlays, function(config, overlayName) {
+                var btn = new Element('button', {
+                    text: overlayName,
+                    'class': 'btn ',
+                    events: {
+                        click: function() {
+                            console.log(this.hasClass('active'));
+                            this.toggleClass('active');
+                        }
+                    }
+                });
+                var listItem = new Element('li');
+                listItem.grab(btn).inject(overlayList);
             });
         },
 
@@ -44,6 +71,15 @@ define([
             options = options || false;
 
             this._connector.setScreen(screenName, options);
+        },
+
+        _onScreenChange: function(screenName, options) {
+            var screenList = $('screenList');
+            Array.each(screenList.getElements('button'), function(button) {
+                var buttonScreen = button.getProperty('data-screen');
+                button.removeClass('btn-warning');
+                button.toggleClass('btn-success', (buttonScreen == screenName));
+            });
         }
     });
 
