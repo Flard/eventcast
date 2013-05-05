2e0b23461db6452fe57a50c58ea1671e0e068396
Grad van Horck <git@gradje.nl>
2013-05-05 13:22:23 +0200

    Started work on facebook comment stream

--- public/js/plugin/facebook/FacebookCommentScreen.js
+++ public/js/plugin/facebook/FacebookCommentScreen.js
@@ -1,15 +1,62 @@
-define(['plugin/AssetManager', 'base/BaseScreen'], function() {
+define(['plugin/VariableManager', 'plugin/AssetManager', 'base/BaseScreen'], function(variableManager) {
     EventCast.Screens.FacebookCommentScreen = new Class({
         Extends: EventCast.BaseScreen,
+        Implements: [ Options ],
+        options: {
+            interval: 10000,
+            messagesPerLoad: 10
+        },
 
         initialize: function() {
             this.parent('facebookcommentstream');
         },
 
         _render: function(canvas) {
-            var screen = new Element('div', { class: 'screen facebookcommentstream'});
+            var screen = new Element('div', {
+                class: 'screen facebookcommentstream',
+                text: variableManager.get('facebook.comment.id')
+            });
+
+            var url = 'https://graph.facebook.com/40796308305_10152387138698306/comments';
+            this._loadComments(url);
+
             screen.inject(canvas);
             return screen;
+        },
+
+        _loadComments: function(url) {
+            var self = this;
+
+            this._request = new Request.JSONP({
+                url: url,
+                onSuccess: function(data) {
+                    console.log(data);
+                    self._parseResults(data.data);
+
+                    var url = self._baseUrl+data.refresh_url;
+                    self._queueRequest(url);
+                },
+                onError: function() {
+                    EventCast.warn('Could not load facebook messages!');
+                    self._queueRequest(url);
+                }
+            })
+            this._request.send();
+        },
+
+        _parseResults: function(tweets) {
+            var self = this;
+            tweets.each(function(tweetData) {
+                var message = new EventCast.TwitterMessage(tweetData);
+                self.addMessage(message);
+            })
+        },
+
+        _queueRequest: function(url, interval) {
+            var self = this;
+            this._timer = window.setTimeout(function() {
+                self._loadComments(url);
+            }, interval || this.options.interval)
         }
     });
 
